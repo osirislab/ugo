836c14bcd919a97d21e13e64dd757572f3bcbade
Kent Ma <tnekent@gmail.com>
2017-12-18 18:13:58 -0500

    Generating cexpr result value for recovered functions

--- src/calling_convention.py
+++ src/calling_convention.py
@@ -1,46 +1,86 @@
 import traceback
-import idaapi
-import idautils
-import idc
+from idaapi import *
+from idautils import *
+from idc import *
+
 import ugo
+
+def go_build_comma_expr_tree(args):
+    lexpr = None
+    argc = len(args)
+    if argc < 2:
+        lexpr = cexpr_t(cot_helper, None)
+        lexpr.helper = "a0"
+        lexpr.exflags = EXFL_ALONE
+    else:
+        lexpr = cexpr_t(cot_comma, None)
+        lexpr.exflags = EXFL_LVALUE
+        descend = lexpr
+
+        for i in range(len(args)):
+            fake = cexpr_t(cot_helper, None)
+            fake.exflags = EXFL_ALONE
+
+            var_name = "a0" + str(i)
+
+            fake.helper = var_name
+            if descend._get_x() is None:
+                descend._set_x(fake)
+            elif descend._get_y() is None:
+                descend._set_y(fake)
+            else:
+                ncomma = cexpr_t(cot_comma, None)
+                ncomma.exflags = EXFL_ALONE
+
+                old_right = descend._get_y()
+                descend._set_y(ncomma)
+                ncomma._set_x(old_right)
+                ncomma._set_y(fake)
+                descend = ncomma
+        lexpr = descend
+
+    return lexpr
+
 def param_fix(func_ea):
     func_struc = ugo.types.func_itabs[func_ea]
     num_params = func_struc["npcdata"]
 
-    tif = idaapi.tinfo_t()
-    idaapi.get_tinfo2(func_ea, tif)
-    funcdata = idaapi.func_type_data_t()
+    tif = tinfo_t()
+    get_tinfo2(func_ea, tif)
+    funcdata = func_type_data_t()
     tif.get_func_details(funcdata)
     new_params = ", ".join("%s %s" %(
-            idaapi.print_tinfo('', 0, 0, idaapi.PRTYPE_1LINE, funcdata[i].type, '', ''),
-            funcdata[i].name
+            print_tinfo('', 0, 0, PRTYPE_1LINE, funcdata[-i].type, '', ''),
+            funcdata[-i].name
         ) for i in range(num_params))
 
+
     # lol this is super jank
-    func_name = idc.GetFunctionName(func_ea).replace(".", "_")
+    func_name = GetFunctionName(func_ea).replace(".", "_")
+    go_build_comma_expr_tree()
+    new_type = "void __usercall %s(%s);" %(func_name, new_params)
+    
+    # lol this is super jank x2
+    new_type = new_type.replace("__interface_{}", "__uint64")
+    SetType(func_ea, new_type)
 
-    new_type = "void __cdecl %s(%s);" %(func_name, new_params)
-    #print "%s -> %s" %(tif, new_type)
-    idc.SetType(func_ea, new_type)
+    print "%s -> %s" %(tif, new_type)
 
 
-class cblock_visitor_t(idaapi.ctree_visitor_t):
+class func_finder(ctree_visitor_t):
     def __init__(self):
-        idaapi.ctree_visitor_t.__init__(self, idaapi.CV_PARENTS)
+        ctree_visitor_t.__init__(self, CV_PARENTS)
         return
 
     def visit_insn(self, ins, *args):
         return 0
 
     def visit_expr(self, expr, *args):
-        if expr.op == idaapi.cot_call:
-            func_name = idc.GetFunctionName(expr.x.obj_ea)
+        if expr.op == cot_call:
+            func_name = GetFunctionName(expr.x.obj_ea)
             if func_name != "":
-                func_struc = ugo.types.func_itabs[expr.x.obj_ea]
-                num_ret_vals = func_struc["nfuncdata"]
                 param_fix(expr.x.obj_ea)
         return 0
-
 
 
 class hexrays_callback_info(object):
@@ -48,26 +88,25 @@ class hexrays_callback_info(object):
         return None
 
     def event_callback(self, event, *args):
-        if event == idaapi.hxe_maturity:
-            print("callback hit")
+        if event == hxe_maturity:
             cfunc, maturity = args
 
-            if maturity == idaapi.CMAT_BUILT:
-                cbv = cblock_visitor_t()
-                cbv.apply_to(cfunc.body, None)
-        
+#            if maturity == CMAT_FINAL:
+            cbv = func_finder()
+            cbv.apply_to(cfunc.body, None)
         return 0
 
 def go_decomp(ea=None):
-    if idaapi.init_hexrays_plugin():
-        idc.GetLongPrm(idc.INF_COMPILER).size_i = 4
+    if init_hexrays_plugin():
+        GetLongPrm(INF_COMPILER).size_i = 4
         i = hexrays_callback_info()
-        idaapi.install_hexrays_callback(i.event_callback)
+        install_hexrays_callback(i.event_callback)
 
         if ea == None:
-            ea = idc.ScreenEA()
-        decomp = idaapi.decompile(ea)
-        idaapi.remove_hexrays_callback(i.event_callback)
+            ea = ScreenEA()
+
+        decomp = decompile(ea)
+        remove_hexrays_callback(i.event_callback)
 
         return decomp
     else:
