04e0de4770a289bf1660ac608a9dd72cc2379669
Kent Ma <tnekent@gmail.com>
2017-12-18 06:02:40 -0500

    Grabbing function information

--- src/calling_convention.py
+++ src/calling_convention.py
@@ -1,44 +1,40 @@
+import traceback
 import idaapi
 import idautils
 import idc
+import ugo
 
-#class cblock_visitor_t(idaapi.ctree_visitor_t):
-#    def __init__(self):
-#        idaapi.ctree_visitor_t.__init__(self, idaapi.CV_PARENTS)
-#        return
-#
-#    def visit_insn(self, ins):
-#        return 0
-#
-#    def visit_expr(self, expr):
-#        if expr.op != idaapi.cot_call:
-#            print(expr)
-#            print(dir(expr))
-#            return 0
-#
-#        return 0
+class cblock_visitor_t(idaapi.ctree_visitor_t):
+    def __init__(self):
+        idaapi.ctree_visitor_t.__init__(self, idaapi.CV_PARENTS)
+        return
+
+    def visit_insn(self, ins, *args):
+        return 0
+
+    def visit_expr(self, expr, *args):
+        if expr.op == idaapi.cot_call:
+            func_struc = ugo.structs.load_struct(expr.x.obj_ea, "_func_itab")
+#            function_name = idc.GetFunctionName(expr.x.obj_ea)
+            function_name = GetFunctionName(expr.x.obj_ea)
+            print(function_name, func_struc)
+            print("alist", list(expr.a))
+        return 0
+
+
 
 class hexrays_callback_info(object):
     def __init__(self):
         return None
 
     def event_callback(self, event, *args):
-        print("callback hit")
-        try:
-            print(event)
-            if event == idaapi.hxe_maturity:
-                cfunc, maturity = args
-                print("callback here")
+        if event == idaapi.hxe_maturity:
+            print("callback hit")
+            cfunc, maturity = args
 
-                if maturity == idaapi.CMAT_BUILT:
-                    print("cfunc", cfunc)
-                    print("cfuncbody", cfunc.body)
-#                    cbv = cblock_visitor_t()
-#                    cbv.apply_to(cfunc.body, None)
-
-        except:
-            print("wtf")
-            traceback.print_exc()
+            if maturity == idaapi.CMAT_FINAL:
+                cbv = cblock_visitor_t()
+                cbv.apply_to(cfunc.body, None)
         
         return 0
 
